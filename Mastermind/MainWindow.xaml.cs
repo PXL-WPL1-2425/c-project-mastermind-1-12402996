3e31b795c35c1eddd3b7d45c258ed4878fefdca9
Berke Özdemir <12402996@student.pxl.be>
2024-11-15 18:06:08 +0100

    MasterMind-02

---
+++ Mastermind/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace C_mastermindSprint1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void GenerateRandomCode()
+        {
+            var randomCode = new Random();
+            List<string> colors = new List<string> {"Rood", "Geel", "Oranje", "Wit", "Groen", "Blauw"};
+            var secretCode = new List<string>();
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                secretCode.Add(colors[randomCode.Next(colors.Count)]);
+
+            }
+                this.Title = "Secret code: " + string.Join(", ", secretCode);
+             
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            GenerateRandomCode();            
+        }
+    }
+}
